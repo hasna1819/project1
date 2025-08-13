fcebf69bfae9d9cfed0112ee68f65da4358a0fc6
mhdrasil <mhdrasil2k7@gmail.com>
2025-08-13 13:07:49 +0530

    new

---
+++ script.js
@@ -0,0 +1,5 @@
+const menuOpenButton=document.querySelector("#menu-open-button")
+menuOpenButton.addEventListener("click"() => {
+    //*toggle mobile menu visibility
+    docucment.body.classlist.toggle("sghow-mobile-menu");
+}
